1fb8a5c01efcf7defe9e94c06dda8c1dd0d46932
Lelouchski <valentin.sepre@gmail.com>
2024-06-20 11:09:16 +0200

    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+const lightMode = document.getElementById('lightmode');
+
+        lightMode.addEventListener('click', function () {
+            const currentTheme = document.documentElement.getAttribute('data-bs-theme')
+            
+            if (currentTheme === 'dark') {
+                document.documentElement.setAttribute('data-bs-theme', 'none')
+            } else {
+                document.documentElement.setAttribute('data-bs-theme', 'dark')
+            }
+        })

6e0cd12daf6b6d6fbfcfdaa02cd4a047f7b7455c
Lelouchski <valentin.sepre@gmail.com>
2024-06-20 16:51:59 +0200

    non

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const lightMode = document.getElementById('lightmode');
+const lightMode = document.getElementById('lightmode')
 
-        lightMode.addEventListener('click', function () {
-            const currentTheme = document.documentElement.getAttribute('data-bs-theme')
-            
-            if (currentTheme === 'dark') {
-                document.documentElement.setAttribute('data-bs-theme', 'none')
-            } else {
-                document.documentElement.setAttribute('data-bs-theme', 'dark')
-            }
-        })
+lightMode.addEventListener('click', function () {
+    const currentTheme = document.documentElement.getAttribute('data-theme')
+
+    if (currentTheme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light')
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark')
+    }
+})

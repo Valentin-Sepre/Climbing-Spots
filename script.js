f5520e6b2b91727aa1b69ed3d939959c56576f4e
Valentin-Sepre <valentin.sepre@gmail.com>
2024-06-21 14:45:15 +0200

    ok

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const lightMode = document.getElementById('lightmode')
 
 lightMode.addEventListener('click', function () {
-    const currentTheme = document.documentElement.getAttribute('data-theme')
+    const currentTheme = document.documentElement.getAttribute('data-bs-theme')
 
     if (currentTheme === 'dark') {
-        document.documentElement.setAttribute('data-theme', 'light')
+        document.documentElement.setAttribute('data-bs-theme', 'light')
     } else {
-        document.documentElement.setAttribute('data-theme', 'dark')
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
     }
 })
